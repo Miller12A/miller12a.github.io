49a2c1c6229d9e32b3b32472084d438e8d328915
Miller Mora <moramiller12345@gmail.com>
2023-05-31 01:18:30 -0500

    Cambio en el curriculum y en la parte de java scrip

--- scrip.js
+++ scrip.js
@@ -1,31 +1,41 @@
 let menuVisible = false;
-//FUNCION QUE OCULTA O MUESTRA EL MENU
+//Función que oculta o muestra el menu
 function mostrarOcultarMenu(){
     if(menuVisible){
-        document.getElementById("nav").classlist ="";
+        document.getElementById("nav").classList ="";
         menuVisible = false;
-    }else
-        document.getElementById("nav").classlist ="responsive";
+    }else{
+        document.getElementById("nav").classList ="responsive";
         menuVisible = true;
+    }
 }
 
-//FUNCION QUE APLICA LAS ANIMACIONES DE LAS HABILIDADES
+function seleccionar(){
+    //oculto el menu una vez que selecciono una opcion
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+//Funcion que aplica las animaciones de las habilidades
 function efectoHabilidades(){
     var skills = document.getElementById("skills");
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >= 300){
         let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList,add("progreso javascript");
-        habilidades[1].classList,add("progreso html");
-        habilidades[2].classList,add("progreso css");
-        habilidades[3].classList,add("progreso python");
-        habilidades[4].classList,add("progreso comunicacion");
-        habilidades[5].classList,add("progreso trabajo");
-        habilidades[6].classList,add("progreso creatividad");
-        habilidades[7].classList,add("progreso dedicacion");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("photoshop");
+        habilidades[3].classList.add("wordpress");
+        habilidades[4].classList.add("drupal");
+        habilidades[5].classList.add("comunicacion");
+        habilidades[6].classList.add("trabajo");
+        habilidades[7].classList.add("creatividad");
+        habilidades[8].classList.add("dedicacion");
+        habilidades[9].classList.add("proyect");
     }
 }
-//DETECTA EL SCROLLING PARA APLICAR LA ANIMACION DE LA BARRA DE HABILIDADES
+
+
+//detecto el scrolling para aplicar la animacion de la barra de habilidades
 window.onscroll = function(){
     efectoHabilidades();
-}
+} 
