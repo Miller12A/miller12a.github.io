f3723f676b4903bef7bc7a9007a1af58f3079bbb
Miller Mora <moramiller12345@gmail.com>
2023-06-05 23:30:09 -0500

    agregamos cambios en los proyectos

--- scrip.js
+++ scrip.js
@@ -21,16 +21,16 @@ function efectoHabilidades(){
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >= 300){
         let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript");
-        habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("photoshop");
-        habilidades[3].classList.add("wordpress");
-        habilidades[4].classList.add("drupal");
-        habilidades[5].classList.add("comunicacion");
-        habilidades[6].classList.add("trabajo");
-        habilidades[7].classList.add("creatividad");
-        habilidades[8].classList.add("dedicacion");
-        habilidades[9].classList.add("proyect");
+        habilidades[0].classList.add("progreso javascript");
+        habilidades[1].classList.add("progreso html");
+        habilidades[2].classList.add("progreso css");
+        habilidades[3].classList.add("progreso python");
+        habilidades[4].classList.add("progreso sql");
+        habilidades[5].classList.add("progreso git");
+        habilidades[6].classList.add("progreso comunicacion");
+        habilidades[7].classList.add("progreso trabajo");
+        habilidades[8].classList.add("progreso creatividad");
+        habilidades[9].classList.add("progreso dedicacion");
     }
 }
 
